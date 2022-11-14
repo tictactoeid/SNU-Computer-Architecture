8f344c36df7ac2d31ef7a935ef7678e4967bdbeb
tictactoeid <tictactoeid@gmail.com>
2022-11-14 18:18:23 +0900

    init

---
+++ Assignment3/bmpresize.s
@@ -0,0 +1,41 @@
+#----------------------------------------------------------------
+#
+#  4190.308 Computer Architecture (Fall 2022)
+#
+#  Project #3: Image Resizing in RISC-V Assembly
+#
+#  November 20, 2022
+# 
+#  Seongyeop Jeong (seongyeop.jeong@snu.ac.kr)
+#  Jaehoon Shim (mattjs@snu.ac.kr)
+#  IlKueon Kang (kangilkueon@snu.ac.kr)
+#  Wookje Han (gksdnrwp@snu.ac.kr)
+#  Jinsol Park (jinsolpark@snu.ac.kr)
+#  Systems Software & Architecture Laboratory
+#  Dept. of Computer Science and Engineering
+#  Seoul National University
+#
+#----------------------------------------------------------------
+
+####################
+# void bmpresize(unsigned char *imgptr, int h, int w, int k, unsigned char *outptr)
+####################
+
+	.globl bmpresize
+bmpresize:
+
+	# TODO
+
+
+
+
+
+
+
+
+
+
+
+
+	ret
+
