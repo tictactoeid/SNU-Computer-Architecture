4c61edc3d5845904189b0c6eec4387a894e85982
tictactoeid <tictactoeid@gmail.com>
2022-12-06 23:50:38 +0900

    push works without forwarding

---
+++ Assignment4/asm/custom.s
@@ -0,0 +1,52 @@
+#==========================================================================
+#
+#   Project #4: Extending a 5-stage Pipelined RISC-V Processor
+#
+#   November 28, 2022
+# 
+#   Seongyeop Jeong (seongyeop.jeong@snu.ac.kr)
+#   Jaehoon Shim (mattjs@snu.ac.kr)
+#   IlKueon Kang (kangilkueon@snu.ac.kr)
+#   Wookje Han (gksdnrwp@snu.ac.kr)
+#   Jinsol Park (jinsolpark@snu.ac.kr)
+#
+#   Systems Software & Architecture Laboratory
+#   Dept. of Computer Science and Engineering
+#   Seoul National University
+#
+#==========================================================================
+
+# Custom: For test push and pop without forwarding
+
+    .text
+    .align  2
+    .globl  _start
+_start:                         # code entry point
+    lui     sp, 0x80020
+    addi    t0, x0, 1
+    addi    t1, x0, 2
+    addi    t2, x0, 3
+    push    t0
+    nop
+    nop
+    nop
+    nop
+    nop
+    push    t1
+    nop
+    nop
+    nop
+    nop
+    nop
+    push    t2
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    #pop     t1
+    ebreak
+    
+
+
